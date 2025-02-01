29e55a438ea7b52c11c634fe495c7318eeb4bd60
SamGrigoli <122939984+SamGrigoli@users.noreply.github.com>
2025-02-01 13:27:45 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 /*
-** client.c -- a stream socket client demo
+** client.c -- A simple TCP client for a number guessing game
 */
 
 #include <stdio.h>
@@ -11,26 +11,22 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
 
-#define PORT "3490" // the port client will be connecting to 
+#define PORT "3490"
+#define MAXDATASIZE 100
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
-
-// get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
-
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, numbytes;  
+    int sockfd, numbytes;
     char buf[MAXDATASIZE];
     struct addrinfo hints, *servinfo, *p;
     int rv;
@@ -50,10 +46,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("client: socket");
             continue;
         }
@@ -71,7 +65,8 @@ int main(int argc, char *argv[])
         fprintf(stderr, "client: failed to connect\n");
         return 2;
     }
-     freeaddrinfo(servinfo);
+
+    freeaddrinfo(servinfo);
 
     while ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) > 0) {
         buf[numbytes] = '\0';
@@ -83,10 +78,7 @@ int main(int argc, char *argv[])
         fgets(buf, sizeof(buf), stdin);
         send(sockfd, buf, strlen(buf), 0);
     }
-    
-    }
 
     close(sockfd);
-
     return 0;
 }
