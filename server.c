6b1560fa1621a2b9370264b136d0bbeb74138561
SamGrigoli <122939984+SamGrigoli@users.noreply.github.com>
2025-02-01 13:26:36 -0500

    Update server.c

--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 /*
-** server.c -- a stream socket server demo
+** server.c -- A simple TCP server for a number guessing game
 */
 
 #include <stdio.h>
@@ -14,66 +14,57 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <time.h>
 
-#define PORT "3490"  // the port users will be connecting to
-
-#define BACKLOG 10   // how many pending connections queue will hold
+#define PORT "3490"  // Port for clients to connect
+#define BACKLOG 10    // Number of pending connections queue will hold
 
 void sigchld_handler(int s)
 {
-    // waitpid() might overwrite errno, so we save and restore it:
     int saved_errno = errno;
-
-    while(waitpid(-1, NULL, WNOHANG) > 0);
-
+    while (waitpid(-1, NULL, WNOHANG) > 0);
     errno = saved_errno;
 }
 
-
-// get sockaddr, IPv4 or IPv6:
+// Get sockaddr, IPv4 or IPv6
 void *get_in_addr(struct sockaddr *sa)
 {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
-
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(void)
 {
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    int sockfd, new_fd;
     struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
+    struct sockaddr_storage their_addr;
     socklen_t sin_size;
     struct sigaction sa;
-    int yes=1;
+    int yes = 1;
     char s[INET6_ADDRSTRLEN];
     int rv;
 
-    //Seed random number generator
-    srand(time(NULL)); 
+    srand(time(NULL));  // Seed random number generator
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    hints.ai_flags = AI_PASSIVE;
 
     if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
             perror("setsockopt");
             exit(1);
         }
@@ -87,9 +78,9 @@ int main(void)
         break;
     }
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo);
 
-    if (p == NULL)  {
+    if (p == NULL) {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
@@ -99,7 +90,7 @@ int main(void)
         exit(1);
     }
 
-    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
@@ -109,7 +100,7 @@ int main(void)
 
     printf("server: waiting for connections...\n");
 
-    while(1) {  // main accept() loop
+    while (1) {
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
         if (new_fd == -1) {
@@ -117,21 +108,43 @@ int main(void)
             continue;
         }
 
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
         printf("server: got connection from %s\n", s);
 
-        if (!fork()) { // this is the child process
-            close(sockfd); // child doesn't need the listener
+        if (!fork()) { 
+            close(sockfd);
             int target = rand() % 100 + 1;
             char buffer[10];
             int guess;
 
-            send(new_fd, "Guess a number between 1 and 100:\n", 35,0);
+            send(new_fd, "Guess a number between 1 and 100:\n", 35, 0);
+
             while (1) {
                 int bytes_received = recv(new_fd, buffer, sizeof(buffer) - 1, 0);
                 if (bytes_received <= 0) break;
+
+                buffer[bytes_received] = '\0';
+                guess = atoi(buffer);
+
+                if (guess < target) {
+                    send(new_fd, "Too low. Try again:\n", 21, 0);
+                } else if (guess > target) {
+                    send(new_fd, "Too high. Try again:\n", 22, 0);
+                } else {
+                    send(new_fd, "Correct! You win!\n", 19, 0);
+                    break;
+                }
+            }
+
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);
+    }
+
+    return 0;
+}
+
 
                 buffer[bytes_received] = '\0';
                 guess = atoi(buffer);
